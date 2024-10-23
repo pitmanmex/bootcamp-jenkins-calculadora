077fc39dfca43298109cef6e15fedab004907f19
pitmanmex <pietromineralle@gmail.com>
2024-10-23 17:39:51 -0600

    Update Calculadora.java

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -2,6 +2,16 @@ package calculadora;
 
 public interface Calculadora {
 	
-	int calcular(int i1, char op, int i2);
+	int calcular(int i1, char op, int i2) {
+		switch (op) {
+			case '+':
+				return i1+i2;
+			case '-':
+				return i1-i2;
+			default:
+				returt -999;
+		}
+	}
+		
 
 }
