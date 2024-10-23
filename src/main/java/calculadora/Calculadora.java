df96ecfdbfbf400b205b749886d32699ad4ed104
pitmanmex <pietromineralle@gmail.com>
2024-10-23 17:42:43 -0600

    Update Calculadora.java
    se agrega main

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -1,6 +1,13 @@
 package calculadora;
 
 public interface Calculadora {
+
+
+public static void main(String[] args) {
+    System.out.println(calcular(5, '+', 3)); // Imprime: 8
+    System.out.println(calcular(5, '-', 3)); // Imprime: 2
+    System.out.println(calcular(5, '*', 3)); // Imprime: -999 (operador no soportado)
+}
 	
 	int calcular(int i1, char op, int i2) {
 		switch (op) {
