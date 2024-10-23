6766bc5671cd682790f4b620cd9d7c232d8039e7
pitmanmex <pietromineralle@gmail.com>
2024-10-23 17:51:12 -0600

    Update Calculadora.java
    interface

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -1,24 +1,5 @@
 package calculadora;
 
 public interface Calculadora {
-
-
-public static void main(String[] args) {
-    System.out.println(calcular(5, '+', 3)); // Imprime: 8
-    System.out.println(calcular(5, '-', 3)); // Imprime: 2
-    System.out.println(calcular(5, '*', 3)); // Imprime: -999 (operador no soportado)
-}
-	
-	int calcular(int i1, char op, int i2) {
-		switch (op) {
-			case '+':
-				return i1+i2;
-			case '-':
-				return i1-i2;
-			default:
-				returt -999;
-		}
-	}
-		
-
+	int calcular(int i1, char op, int i2);
 }
