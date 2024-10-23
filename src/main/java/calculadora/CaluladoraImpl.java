cfa55ed98a5ab928b25b7c05071774083ad03475
pitmanmex <pietromineralle@gmail.com>
2024-10-23 17:51:50 -0600

    Update CaluladoraImpl.java
    calcular

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -2,9 +2,25 @@ package calculadora;
 
 public class CaluladoraImpl implements Calculadora {
 	
-	@Override
-	public int calcular(int i1, char op, int i2) {
-		return 0;
+
+public static void main(String[] args) {
+    System.out.println(calcular(5, '+', 3)); // Imprime: 8
+    System.out.println(calcular(5, '-', 3)); // Imprime: 2
+    System.out.println(calcular(5, '*', 3)); // Imprime: -999 (operador no soportado)
+}
+	
+	@Override	
+	int calcular(int i1, char op, int i2) {
+		switch (op) {
+			case '+':
+				return i1+i2;
+			case '-':
+				return i1-i2;
+			default:
+				returt -999;
+		}
 	}
+		
+
 
 }
