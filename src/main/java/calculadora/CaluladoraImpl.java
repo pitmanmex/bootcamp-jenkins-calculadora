49ea5d48aa535254399eaa46b1c9d7104774242d
pitmanmex <pietromineralle@gmail.com>
2024-10-23 18:20:22 -0600

    Update CaluladoraImpl.java

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -3,7 +3,7 @@ package calculadora;
 public class CaluladoraImpl implements Calculadora {
 	
 	@Override
-int calcular(int i1, char op, int i2) {
+private int calcular(int i1, char op, int i2) {
 		switch (op) {
 			case '+':
 				return i1+i2;
