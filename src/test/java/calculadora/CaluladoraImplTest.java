4600b02b272fea3b0ad5a368d2eecd276164d41d
pitmanmex <pietromineralle@gmail.com>
2024-10-28 17:55:06 -0600

    Update CaluladoraImplTest.java

--- src/test/java/calculadora/CaluladoraImplTest.java
+++ src/test/java/calculadora/CaluladoraImplTest.java
@@ -17,6 +17,8 @@ class CaluladoraImplTest {
 	void test_debe_sumar_cuando_se_utiliza_mas() {
 		int resultado = calculadora.calcular(1, '+', 2);
 		Assertions.assertEquals(3, resultado);
+		int resultado = calculadora.calcular(1, '-', 2);
+		Assertions.assertEquals(-1, resultado);
 	}
 
 }
